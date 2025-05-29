bbaa0f3ba2d1ae3e2e540cd42017324d59f90f95
DESKTOP-3GUQ93R\GA2A <h1202419001@hiratagakuen.onmicrosoft.com>
2025-05-29 16:33:51 +0900

    マップ表示

---
+++ teamproject/Assets/aM/Script_M/MapCamera_M.cs
@@ -0,0 +1,24 @@
+using System.Drawing;
+using UnityEngine;
+
+public class MapCamera_M : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform camera;
+
+    Vector3 pos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = player.transform.position;
+        pos.y = camera.transform.position.y;
+        camera.transform.position = pos;
+    }
+}
