77c9637168af849a7a3fdfdb0f15db427a22f895
DESKTOP-3GUQ93R\GA2A <h1202419001@hiratagakuen.onmicrosoft.com>
2025-06-16 16:45:36 +0900

    ポーズ画面のセレクターの動きを作成

--- teamproject/Assets/aM/Script_M/PoseManager_M.cs
+++ teamproject/Assets/aM/Script_M/PoseManager_M.cs
@@ -6,9 +6,11 @@ public class PoseManager_M : MonoBehaviour
 {
     public int showing_pose;//ポーズが表示されてるか
 
-    public GameObject pose_obj;//ポーズキャンバスを読み込む
+    [SerializeField] GameObject pose_obj;//ポーズキャンバスを読み込む
+    [SerializeField] GameObject game_obj;//ゲームキャンバスを読み込む
     InputAction poseAction;
     InputAction selectorAction;
+    InputAction poseClick;
 
     Vector2 selectValue;
 
@@ -16,10 +18,14 @@ public class PoseManager_M : MonoBehaviour
 
     public int selector_pos = 0;
 
+    bool push_selector = false;
+
     [SerializeField] Image ReStart;
     [SerializeField] Image Status;
     [SerializeField] Image OperationMethod;
     [SerializeField] Image FinishStage;
+
+    [SerializeField] GameObject Frame;
 
     enum Selector_ID
     {
@@ -35,6 +41,7 @@ public class PoseManager_M : MonoBehaviour
         showing_pose = 0;
         poseAction = InputSystem.actions.FindAction("Pose");
         selectorAction = InputSystem.actions.FindAction("PoseSelect");
+        poseClick = InputSystem.actions.FindAction("PoseClick");
     }
 
     // Update is called once per frame
@@ -56,7 +63,7 @@ public class PoseManager_M : MonoBehaviour
             Time.timeScale = 0.0f;
             selectValue = selectorAction.ReadValue<Vector2>();
             //左スティックが上下に倒されたらセレクトの切り替えを行う
-            if (selectValue.y != 0)
+            if (selectValue.y != 0 && !push_selector)
             {
                 if (selector_move_time >= 15) selector_move_time = 0;
                 if (selectValue.y > 0.5 && selector_move_time == 0)
@@ -77,13 +84,18 @@ public class PoseManager_M : MonoBehaviour
             {
                 selector_move_time = 0;
             }
-            if(selectValue.x > 0.5)
+
+            //決定ボタンが押されたら
+            if (poseClick.WasPressedThisFrame())
             {
-                selector_pos = 0;
+                push_selector = true;
+                Click_Select(selector_pos);
             }
-            if(selectValue.x < -0.5)
+            //決定ボタンが離されたら
+            if(poseClick.WasReleasedThisFrame())
             {
-                selector_pos = 3;
+                push_selector = false;
+                Remove_Color(selector_pos);
             }
         }
         else
@@ -97,44 +109,77 @@ public class PoseManager_M : MonoBehaviour
     {
         if (showing == 0)
         {
+            ReStart.color = new Color32(255, 255, 255, 255);
             pose_obj.SetActive(true);
+            game_obj.SetActive(false);
             Debug.Log("1");
         }
         else
         {
             pose_obj.SetActive(false);
+            game_obj.SetActive(true);
             Debug.Log("2");
         }
     }
 
-    //
+    //セレクターフレームの位置を変更する関数
     void Show_Selector(int spos)
     {
         switch(spos)
         {
             case (int)Selector_ID.RESTART:
-                ReStart.color = new Color32(100, 100, 100, 255);
-                Status.color = new Color32(255,255,255,255);
-                OperationMethod.color = new Color32(255, 255, 255, 255);
-                FinishStage.color = new Color32(255, 255, 255, 255);
+                Frame.transform.position = ReStart.transform.position;
                 break;
             case (int)Selector_ID.STATUS:
-                ReStart.color = new Color32(255, 255, 255, 255);
-                Status.color = new Color32(100, 100, 100, 255);
-                OperationMethod.color = new Color32(255, 255, 255, 255);
-                FinishStage.color = new Color32(255, 255, 255, 255);
+                Frame.transform.position = Status.transform.position;
                 break;
             case (int)Selector_ID.OPERATIONMETHOD:
-                ReStart.color = new Color32(255, 255, 255, 255);
-                Status.color = new Color32(255, 255, 255, 255);
-                OperationMethod.color = new Color32(100, 100, 100, 255);
-                FinishStage.color = new Color32(255, 255, 255, 255);
+                Frame.transform.position = OperationMethod.transform.position;
                 break;
             case (int)Selector_ID.FINISHSTAGE:
-                ReStart.color = new Color32(255, 255, 255, 255);
-                Status.color = new Color32(255, 255, 255, 255);
-                OperationMethod.color = new Color32(255, 255, 255, 255);
+                Frame.transform.position = FinishStage.transform.position;
+                break;
+        }
+    }
+
+    //選択された場所の色を暗くする
+    void Click_Select(int spos)
+    {
+        switch (spos)
+        {
+            case (int)Selector_ID.RESTART:
+                ReStart.color = new Color32(100, 100, 100,255);
+                break;
+            case (int)Selector_ID.STATUS:
+                Status.color = new Color32(100, 100, 100, 255);
+                break;
+            case (int)Selector_ID.OPERATIONMETHOD:
+                OperationMethod.color = new Color32(100, 100, 100, 255);
+                break;
+            case (int)Selector_ID.FINISHSTAGE:
                 FinishStage.color = new Color32(100, 100, 100, 255);
+                break;
+        }
+    }
+
+    //時間が過ぎたら色を戻す
+    void Remove_Color(int spos)
+    {
+        switch (spos)
+        {
+            case (int)Selector_ID.RESTART:
+                ReStart.color = new Color32(255, 255, 255, 255);
+                Change_Pose(showing_pose);
+                showing_pose++;
+                break;
+            case (int)Selector_ID.STATUS:
+                Status.color = new Color32(255, 255, 255, 255);
+                break;
+            case (int)Selector_ID.OPERATIONMETHOD:
+                OperationMethod.color = new Color32(255, 255, 255, 255);
+                break;
+            case (int)Selector_ID.FINISHSTAGE:
+                FinishStage.color = new Color32(255, 255, 255, 255);
                 break;
         }
     }
