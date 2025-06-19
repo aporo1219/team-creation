c0dd5093410f9b89e376bd6fccfbc374f418ebcc
DESKTOP-3GUQ93R\GA2A <h1202419001@hiratagakuen.onmicrosoft.com>
2025-06-19 16:34:49 +0900

    ポーズボタンを押したらポーズ画面が動きながら起動するようにした

--- teamproject/Assets/aM/Script_M/PoseManager_M.cs
+++ teamproject/Assets/aM/Script_M/PoseManager_M.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 
 public class PoseManager_M : MonoBehaviour
 {
+    public int x = 0;
+    public int y = 0;
+
     public int showing_pose;//ポーズが表示されてるか
 
     [SerializeField] GameObject pose_obj;       //ポーズキャンバスを読み込む
@@ -23,13 +26,18 @@ public class PoseManager_M : MonoBehaviour
     [SerializeField] Image BackGround;          //ポーズ画面の背景
     int alpha_time = 0;
 
-    [SerializeField] GameObject Map;            //マップ画像
+    [SerializeField] RawImage Map;              //マップ画像
     [SerializeField] Image ReStart;             //再開画像
     [SerializeField] Image Status;              //ステータス画像
     [SerializeField] Image OperationMethod;     //操作確認画像
     [SerializeField] Image FinishStage;         //ステージ終了画像
+    [SerializeField] Image Passive;             //パッシブスキル画像
+    [SerializeField] Image Active;              //アクティブスキル画像
 
     [SerializeField] GameObject Frame;          //セレクターのフレーム画像
+
+    Vector3[] UI_pos;   //UIの位置
+    int pose_start = 0;
 
     enum Selector_ID
     {
@@ -52,12 +60,28 @@ public class PoseManager_M : MonoBehaviour
         selectorAction = InputSystem.actions.FindAction("PoseSelect");
         poseClick = InputSystem.actions.FindAction("PoseClick");
 
-        ReStart.rectTransform.position = new Vector3(100, 0, 0);
+        UI_pos = new Vector3[7];
+
+        UI_pos[0] = new Vector3(-800, 250, 0);
+        UI_pos[1] = new Vector3(-370, 250, 0);
+        UI_pos[2] = new Vector3(-800, 90, 0);
+        UI_pos[3] = new Vector3(-370, 90, 0);
+        UI_pos[4] = new Vector3(-600, 700, 0);
+        UI_pos[5] = new Vector3(2450, 135, 0);
+        UI_pos[6] = new Vector3(2450, 370, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        ReStart.rectTransform.position = new Vector3(UI_pos[0].x, UI_pos[0].y, 0);
+        Status.rectTransform.position = new Vector3(UI_pos[1].x, UI_pos[1].y, 0);
+        OperationMethod.rectTransform.position = new Vector3(UI_pos[2].x, UI_pos[2].y, 0);
+        FinishStage.rectTransform.position = new Vector3(UI_pos[3].x, UI_pos[3].y, 0);
+        Map.rectTransform.position = new Vector3(UI_pos[4].x, UI_pos[4].y, 0);
+        Passive.rectTransform.position = new Vector3(UI_pos[5].x, UI_pos[5].y, 0);
+        Active.rectTransform.position = new Vector3(UI_pos[6].x, UI_pos[6].y, 0);
+
         //Escapeキーの入力でポーズ画面の切り替えを行う
         //コントローラーのポーズボタンでポーズ画面の切り替えを行う
         if (Input.GetKeyDown(KeyCode.Escape) || poseAction.WasPressedThisFrame())
@@ -86,55 +110,97 @@ public class PoseManager_M : MonoBehaviour
         //ポーズ画面中の行動
         if (showing_pose == 1)
         {
-            //ポーズ画面中はゲーム内の動きを止める
-            Time.timeScale = 0.0f;
-            //左スティックの力の動きを取ってくる
-            selectValue = selectorAction.ReadValue<Vector2>();
-            //左スティックが上下に倒されたらセレクトの切り替えを行う
-            if (selectValue.y != 0 && !push_selector)
+            if (pose_start < 30)
             {
-                //セレクターの連続の動きを15秒に１度にする
-                if (selector_move_time >= 15) selector_move_time = 0;
-                //左スティックが上に倒されたらカーソルを上に動かす
-                if (selectValue.y > 0.5 && selector_move_time == 0)
+                pose_start++;
+                int i;
+                for (i = 0; i < 5; i++)
                 {
-                    selector_pos--;
-                    if (selector_pos < 0) selector_pos = 0;
-                    Debug.Log("selector_moveup");
+                    UI_pos[i].x += 35;
                 }
-                //左スティックが下に倒されたらカーソルを下に動かす
-                if (selectValue.y < -0.5 && selector_move_time == 0)
+                for (; i < 7; i++)
                 {
-                    selector_pos++;
-                    if (selector_pos > 3) selector_pos = 3;
-                    Debug.Log("selector_movedown");
+                    UI_pos[i].x -= 35;
                 }
-                //左スティックが上下に傾けられたらselector_move_timeを１ずつ増やす
-                if (selectValue.y > 0.5 || selectValue.y < -0.5)
-                    selector_move_time++;
             }
             else
             {
-                //左スティックが傾けられてなかったらselector_move_timeを０にする
-                selector_move_time = 0;
-            }
+                //ポーズ画面中はゲーム内の動きを止める
+                Time.timeScale = 0.0f;
+                //左スティックの力の動きを取ってくる
+                selectValue = selectorAction.ReadValue<Vector2>();
+                //左スティックが倒されたら
+                if ((selectValue.x != 0 || selectValue.y != 0) && !push_selector)
+                {
+                    //セレクターの連続の動きを15秒に１度にする
+                    if (selector_move_time >= 15) selector_move_time = 0;
+                    //左スティックが左に倒されたらカーソルを左に動かす
+                    if (selectValue.x < -0.5 && selector_move_time == 0 && selector_pos != 2)
+                    {
+                        selector_pos--;
+                        if (selector_pos < 0) selector_pos = 0;
+                        Debug.Log("selector_moveleft");
+                    }
+                    //左スティックが右に倒されたらカーソルを右に動かす
+                    if (selectValue.x > 0.5 && selector_move_time == 0 && selector_pos != 1)
+                    {
+                        selector_pos++;
+                        if (selector_pos > 3) selector_pos = 3;
+                        Debug.Log("selector_moveright");
+                    }
+                    //左スティックが上に倒されたらカーソルを上に動かす
+                    if (selectValue.y > 0.5 && selector_move_time == 0)
+                    {
+                        selector_pos -= 2;
+                        if (selector_pos < 0) selector_pos += 2;
+                        Debug.Log("selector_moveup");
+                    }
+                    //左スティックが下に倒されたらカーソルを下に動かす
+                    if (selectValue.y < -0.5 && selector_move_time == 0)
+                    {
+                        selector_pos += 2;
+                        if (selector_pos > 3) selector_pos -= 2;
+                        Debug.Log("selector_movedown");
+                    }
+                    //左スティックが上下に傾けられたらselector_move_timeを１ずつ増やす
+                    if (selectValue.y > 0.5 || selectValue.y < -0.5)
+                        selector_move_time++;
+                }
+                else
+                {
+                    //左スティックが傾けられてなかったらselector_move_timeを０にする
+                    selector_move_time = 0;
+                }
 
-            //決定ボタンが押されたら
-            if (poseClick.WasPressedThisFrame())
-            {
-                push_selector = true;
-                Click_Select(selector_pos);
-            }
-            //決定ボタンが離されたら
-            if (poseClick.WasReleasedThisFrame())
-            {
-                push_selector = false;
-                Remove_Color(selector_pos);
+                //決定ボタンが押されたら
+                if (poseClick.WasPressedThisFrame() && !push_selector)
+                {
+                    push_selector = true;
+                    Click_Select(selector_pos);
+                }
+                //決定ボタンが離されたら
+                if (poseClick.WasReleasedThisFrame() && push_selector)
+                {
+                    push_selector = false;
+                    Remove_Color(selector_pos);
+                }
             }
         }
         //ポーズ画面を離れたらゲーム内を動かす
         else
+        {
             Time.timeScale = 1.0f;
+
+            UI_pos[0] = new Vector3(-800, 250, 0);
+            UI_pos[1] = new Vector3(-370, 250, 0);
+            UI_pos[2] = new Vector3(-800, 90, 0);
+            UI_pos[3] = new Vector3(-370, 90, 0);
+            UI_pos[4] = new Vector3(-600, 700, 0);
+            UI_pos[5] = new Vector3(2450, 135, 0);
+            UI_pos[6] = new Vector3(2450, 370, 0);
+            pose_start = 0;
+            selector_pos = 0;
+        }
 
         //セレクターの位置を表示する
         Show_Selector(selector_pos);
