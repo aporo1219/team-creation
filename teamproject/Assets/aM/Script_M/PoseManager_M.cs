8bf6fa2a6dad6d05dea63a4d5e4f976a25032e2a
DESKTOP-2TIO13Q\GA2A <yugiry>
2025-06-20 16:32:31 +0900

    ポーズ画面のフェードインとフェードアウトの作成

--- teamproject/Assets/aM/Script_M/PoseManager_M.cs
+++ teamproject/Assets/aM/Script_M/PoseManager_M.cs
@@ -14,6 +14,7 @@ public class PoseManager_M : MonoBehaviour
     InputAction poseAction;                     //ポーズ画面に切り替えるためのアクション
     InputAction selectorAction;                 //ポーズ画面内のセレクターの動き
     InputAction poseClick;                      //ポーズ画面で決定ボタンを押した
+    InputAction poseCancel;
 
     Vector2 selectValue;                        //セレクターの上下左右の判定
 
@@ -24,7 +25,13 @@ public class PoseManager_M : MonoBehaviour
     bool push_selector = false;                 //決定ボタンが押されたか
 
     [SerializeField] Image BackGround;          //ポーズ画面の背景
-    int alpha_time = 0;
+    int alpha_time = 0;                         //透明度
+
+    bool Click_UI = false;                      //ステータスか操作説明のUIがクリックされたか
+    int comment_move_time = 0;                  //説明パネルが動く時間
+    bool comment_move = false;                  //説明パネルが動いているか
+    int comment_num = 0;                        //どの説明パネルが開かれているか
+    bool push_cancel = false;                   //キャンセルボタンが押されたか
 
     [SerializeField] RawImage Map;              //マップ画像
     [SerializeField] Image ReStart;             //再開画像
@@ -33,11 +40,14 @@ public class PoseManager_M : MonoBehaviour
     [SerializeField] Image FinishStage;         //ステージ終了画像
     [SerializeField] Image Passive;             //パッシブスキル画像
     [SerializeField] Image Active;              //アクティブスキル画像
+    [SerializeField] Image Comment_Panel;       //説明パネル画像
 
     [SerializeField] GameObject Frame;          //セレクターのフレーム画像
 
-    Vector3[] UI_pos;   //UIの位置
-    int pose_start = 0;
+    [SerializeField] Text Comment_Text;         //説明パネルの文字
+
+    Vector3[] UI_pos;                           //UIの位置
+    int pose_start = 0;                         //ポーズ画面のフェードイン＆フェードアウトの時間
 
     enum Selector_ID
     {
@@ -49,7 +59,7 @@ public class PoseManager_M : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 120;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -59,8 +69,9 @@ public class PoseManager_M : MonoBehaviour
         poseAction = InputSystem.actions.FindAction("Pose");
         selectorAction = InputSystem.actions.FindAction("PoseSelect");
         poseClick = InputSystem.actions.FindAction("PoseClick");
+        poseCancel = InputSystem.actions.FindAction("PoseCancel");
 
-        UI_pos = new Vector3[7];
+        UI_pos = new Vector3[8];
 
         UI_pos[0] = new Vector3(-800, 250, 0);
         UI_pos[1] = new Vector3(-370, 250, 0);
@@ -69,11 +80,13 @@ public class PoseManager_M : MonoBehaviour
         UI_pos[4] = new Vector3(-600, 700, 0);
         UI_pos[5] = new Vector3(2450, 135, 0);
         UI_pos[6] = new Vector3(2450, 370, 0);
+        UI_pos[7] = new Vector3(1400, 1350, 0);
     }
-
+    //1350
     // Update is called once per frame
     void Update()
     {
+        //UIの移動処理
         ReStart.rectTransform.position = new Vector3(UI_pos[0].x, UI_pos[0].y, 0);
         Status.rectTransform.position = new Vector3(UI_pos[1].x, UI_pos[1].y, 0);
         OperationMethod.rectTransform.position = new Vector3(UI_pos[2].x, UI_pos[2].y, 0);
@@ -81,12 +94,14 @@ public class PoseManager_M : MonoBehaviour
         Map.rectTransform.position = new Vector3(UI_pos[4].x, UI_pos[4].y, 0);
         Passive.rectTransform.position = new Vector3(UI_pos[5].x, UI_pos[5].y, 0);
         Active.rectTransform.position = new Vector3(UI_pos[6].x, UI_pos[6].y, 0);
+        Comment_Panel.rectTransform.position = new Vector3(UI_pos[7].x, UI_pos[7].y, 0);
 
         //Escapeキーの入力でポーズ画面の切り替えを行う
         //コントローラーのポーズボタンでポーズ画面の切り替えを行う
         if (Input.GetKeyDown(KeyCode.Escape) || poseAction.WasPressedThisFrame())
         {
-            Change_Pose(showing_pose);
+            if (pose_start == 0)
+                Change_Pose(showing_pose);
             showing_pose++;
         }
 
@@ -101,15 +116,18 @@ public class PoseManager_M : MonoBehaviour
             BackGround.color = new Color32(0, 0, 0, (byte)alpha_time);
         }
         //ポーズ画面から離れたら背景の透明度を０に戻す
-        else if(showing_pose == 0)
+        else if(showing_pose == 0 && alpha_time > 0)
         {
-            alpha_time = 0;
-            BackGround.color = new Color32(0, 0, 0, 0);
+            alpha_time -= 8;
+            BackGround.color = new Color32(0, 0, 0, (byte)alpha_time);
         }
 
         //ポーズ画面中の行動
         if (showing_pose == 1)
         {
+            //ポーズ画面中はゲーム内の動きを止める
+            Time.timeScale = 0.0f;
+            //30フレームの間にUIを表示する
             if (pose_start < 30)
             {
                 pose_start++;
@@ -122,15 +140,14 @@ public class PoseManager_M : MonoBehaviour
                 {
                     UI_pos[i].x -= 35;
                 }
+                UI_pos[i].y -= 19;
             }
             else
             {
-                //ポーズ画面中はゲーム内の動きを止める
-                Time.timeScale = 0.0f;
                 //左スティックの力の動きを取ってくる
                 selectValue = selectorAction.ReadValue<Vector2>();
                 //左スティックが倒されたら
-                if ((selectValue.x != 0 || selectValue.y != 0) && !push_selector)
+                if ((selectValue.x != 0 || selectValue.y != 0) && !push_selector && !Click_UI)
                 {
                     //セレクターの連続の動きを15秒に１度にする
                     if (selector_move_time >= 15) selector_move_time = 0;
@@ -173,33 +190,109 @@ public class PoseManager_M : MonoBehaviour
                 }
 
                 //決定ボタンが押されたら
-                if (poseClick.WasPressedThisFrame() && !push_selector)
+                if (poseClick.WasPressedThisFrame() && !push_selector && !Click_UI)
                 {
                     push_selector = true;
                     Click_Select(selector_pos);
                 }
                 //決定ボタンが離されたら
-                if (poseClick.WasReleasedThisFrame() && push_selector)
+                if (poseClick.WasReleasedThisFrame() && push_selector && !Click_UI)
                 {
                     push_selector = false;
                     Remove_Color(selector_pos);
+                }
+
+                //キャンセルボタンが押されたら
+                if(poseCancel.WasPressedThisFrame() && !push_selector)
+                {
+                    push_cancel = true;
+                }
+                //キャンセルボタンが離されたら
+                if(poseCancel.WasReleasedThisFrame() && push_selector && !push_cancel)
+                {
+                    push_selector = false;
+                }
+                //UIがクリックされたら
+                if (Click_UI && selector_pos != comment_num)
+                {
+                    if (comment_move_time < 30 && !comment_move)
+                    {
+                        comment_move_time++;
+                        UI_pos[7].y += 19;
+                    }
+                    if (comment_move_time > 0 && comment_move)
+                    {
+                        comment_move_time--;
+                        UI_pos[7].y -= 19;
+                    }
+                    if (comment_move_time == 30)
+                    {
+                        comment_move = true;
+                        Change_Text(selector_pos);
+                    }
+                    if (comment_move_time == 0)
+                    {
+                        comment_move = false;
+                        push_selector = false;
+                        comment_num = selector_pos;
+                    }
+                }
+                //UIがキャンセルされたら
+                if(push_cancel)
+                {
+                    push_selector = true;
+                    Click_UI = false;
+                    if (comment_move_time < 30 && !comment_move)
+                    {
+                        comment_move_time++;
+                        UI_pos[7].y += 19;
+                    }
+                    if (comment_move_time > 0 && comment_move)
+                    {
+                        comment_move_time--;
+                        UI_pos[7].y -= 19;
+                    }
+                    if (comment_move_time == 30)
+                    {
+                        comment_move = true;
+                        Change_Text(0);
+                    }
+                    if (comment_move_time == 0)
+                    {
+                        comment_move = false;
+                        push_selector = false;
+                        comment_num = 0;
+                        push_cancel = false;
+                        comment_num = 0;
+                        Remove_Color(selector_pos);
+                    }
                 }
             }
         }
         //ポーズ画面を離れたらゲーム内を動かす
         else
         {
-            Time.timeScale = 1.0f;
-
-            UI_pos[0] = new Vector3(-800, 250, 0);
-            UI_pos[1] = new Vector3(-370, 250, 0);
-            UI_pos[2] = new Vector3(-800, 90, 0);
-            UI_pos[3] = new Vector3(-370, 90, 0);
-            UI_pos[4] = new Vector3(-600, 700, 0);
-            UI_pos[5] = new Vector3(2450, 135, 0);
-            UI_pos[6] = new Vector3(2450, 370, 0);
-            pose_start = 0;
-            selector_pos = 0;
+            //30フレームの間にUIを非表示にする
+            if (pose_start > 0)
+            {
+                pose_start--;
+                int i;
+                for (i = 0; i < 5; i++)
+                {
+                    UI_pos[i].x -= 35;
+                }
+                for (; i < 7; i++)
+                {
+                    UI_pos[i].x += 35;
+                }
+                UI_pos[i].y += 19;
+            }
+            //UIが非表示になったらゲーム内を動かす
+            if(pose_start == 0)
+            {
+                Time.timeScale = 1.0f;
+                selector_pos = 0;
+            }
         }
 
         //セレクターの位置を表示する
@@ -214,13 +307,11 @@ public class PoseManager_M : MonoBehaviour
             ReStart.color = new Color32(255, 255, 255, 255);
             pose_obj.SetActive(true);
             game_obj.SetActive(false);
-            Debug.Log("1");
         }
         else
         {
             pose_obj.SetActive(false);
             game_obj.SetActive(true);
-            Debug.Log("2");
         }
     }
 
@@ -254,9 +345,11 @@ public class PoseManager_M : MonoBehaviour
                 break;
             case (int)Selector_ID.STATUS:
                 Status.color = new Color32(100, 100, 100, 255);
+                Click_UI = true;
                 break;
             case (int)Selector_ID.OPERATIONMETHOD:
                 OperationMethod.color = new Color32(100, 100, 100, 255);
+                Click_UI = true;
                 break;
             case (int)Selector_ID.FINISHSTAGE:
                 FinishStage.color = new Color32(100, 100, 100, 255);
@@ -282,6 +375,25 @@ public class PoseManager_M : MonoBehaviour
                 break;
             case (int)Selector_ID.FINISHSTAGE:
                 FinishStage.color = new Color32(255, 255, 255, 255);
+                break;
+        }
+    }
+
+    //説明パネルの文字を変える
+    void Change_Text(int spos)
+    {
+        switch(spos)
+        {
+            case 0:
+                Comment_Text.text = "テスト１";
+                break;
+            case 1:
+
+                Comment_Text.text = "テスト２";
+                break;
+            case 2:
+
+                Comment_Text.text = "テスト３";
                 break;
         }
     }
