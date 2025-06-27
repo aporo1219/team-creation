d33f4e40c20e7ac86fa86486da6a3274b9e0c89b
DESKTOP-2TIO13Q\GA2A <yugiry>
2025-06-27 16:33:06 +0900

    チュートリアルステージを変更

--- teamproject/Assets/aM/Script_M/TutorialManager.cs
+++ teamproject/Assets/aM/Script_M/TutorialManager.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    public bool tutorial_start = true;
+    public List<bool> tutorial_clear = new List<bool>();
+    [SerializeField] List<GameObject> Tutorial_Obj = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,8 +16,15 @@ public class TutorialManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //チュートリアルを開始する
+        if(tutorial_start)
+        {
+            Now_Tutorial();
+        }
     }
 
-
+    void Now_Tutorial()
+    {
+        
+    }
 }
