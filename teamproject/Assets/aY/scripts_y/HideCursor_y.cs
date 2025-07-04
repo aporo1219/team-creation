e4d9c3203a346712c5d762cb6728a20fab4fab79
DESKTOP-0L17MQ3\GA2A <h1202415021@hiratagakuen.onmicrosoft.com>
2025-07-04 16:33:28 +0900

    プレイヤーのアニメーション微修正・マウスカーソルを非表示にするスクリプト作成・スキル管理のベース作成

---
+++ teamproject/Assets/aY/scripts_y/HideCursor_y.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor_y : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
--- teamproject/Assets/aY/scripts_y/SkillController_y.cs
+++ teamproject/Assets/aY/scripts_y/SkillController_y.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class SkillController_y : MonoBehaviour
 {
+    PlayerController_y1 PlayerCont;
+
+    public GameObject[] EquipActiveSkills = new GameObject[3];
+    public GameObject[] EquipPassiveSkills = new GameObject[3];
+
+    public int SelectSkill;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PlayerCont = GetComponent<PlayerController_y1>();
+
+        SelectSkill = 0;
     }
 
     // Update is called once per frame
