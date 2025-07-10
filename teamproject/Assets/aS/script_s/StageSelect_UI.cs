5c2279ee2d65e8a56eb3c32ba99fb733d26e40af
DESKTOP-3GUQ93R\GA2A <h1202419006@hiratagakuen.onmicrosoft.com>
2025-07-10 16:10:51 +0900

    ステージセレクトの処理の途中

--- teamproject/Assets/aS/script_s/StageSelect_UI.cs
+++ teamproject/Assets/aS/script_s/StageSelect_UI.cs
@@ -8,21 +8,23 @@ using UnityEngine.SceneManagement;
 public class StageSelectUI : MonoBehaviour
 {
     private int f;
-    private int Level_Floor = 1;
-    private string Arrow = "↑";
+    private int Level_Floor = 0;
     private Gamepad GP;
-    //[SerializeField] InputSystem Select_Push;
+    private Color Choice = Color.yellow;
+    private Color Default = Color.white;
 
     [SerializeField] Text Floor_Display;
     [SerializeField] Button[] Button;
     [SerializeField] GameObject[] G_Button;
     [SerializeField] GameObject First_Button;
-
+    [SerializeField] GameObject[] GB;
+    [SerializeField] Material[] Normal_M;
+    [SerializeField] Material[] Change_M;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        FloorDisplay();
+        FloorDisplay(Level_Floor);
         //コントローラとUIボタンの紐づけ
         EventSystem.current.SetSelectedGameObject(First_Button);
 
@@ -33,17 +35,19 @@ public class StageSelectUI : MonoBehaviour
             if (Button[i] == null && G_Button[i] == null)
             {
                 Debug.LogError($"Button[{i}] が設定されていません！");
+                Debug.LogError($"GB[{i}] が設定されていません！");
             }
             else
             {
                 int index = i;
                 Button[i].onClick.AddListener(() => OnButtonPressed(index));
                 //G_Button[i].SetActive(false);
-                if (G_Button[i] == First_Button)
+                if (i == 0)
                 {
                     G_Button[i].SetActive(true);
                 }
                 Debug.Log($"Button[{i}] にリスナー登録完了");
+                Debug.Log($"GB[{i}] にリスナー登録完了");
             }
         }
     }
@@ -59,34 +63,29 @@ public class StageSelectUI : MonoBehaviour
 
     private void FixedUpdate()
     {
+        GameObject Selected = EventSystem.current.currentSelectedGameObject;
         //ゲームパッドの入力
         GP = Gamepad.current;
-        if(GP == null )
+        if (GP == null)
         {
             return;
         }
         Vector2 Right_Stick = GP.rightStick.ReadValue();
 
-        int CF = 1;
-        ReleaseFloor(CF);
-        G_Button[3].SetActive(true);
-    }
+        //選択のマテリアル変更処理
+        for (int i = 0; i < GB.Length; i++)
+        {
+            Renderer R = GB[i].GetComponent<Renderer>();
 
-    //フロアを解放したら次のフロアを解放
-    public void ReleaseFloor (int CF)
-    {
-        //G_Button[3].SetActive(true);
-    }
-
-    public void FloorSet(int floor)
-    {
-        Level_Floor = floor;
-        FloorDisplay();
-    }
-
-    void FloorDisplay()
-    {
-        Floor_Display.text =  Arrow + "B" + Level_Floor ; 
+            if (Selected == G_Button[i])
+            {
+                R.material = Change_M[i];
+            }
+            else
+            {
+                R.material = Normal_M[i];
+            }
+        }
     }
 
     //ボタンが押された時の処理
@@ -96,27 +95,36 @@ public class StageSelectUI : MonoBehaviour
         {
             case 3:
                 Debug.Log("最終ステージへ");
-                Invoke(nameof(ChangeScene), 5.0f);
+                Invoke(nameof(ChangeScene), 0.1f);
                 f = 3;
+                Level_Floor = 10;
+                FloorDisplay(Level_Floor);
                 break;
             case 2:
                 Debug.Log("2ステージへ");
-                Invoke(nameof(ChangeScene), 5.0f);
+                Invoke(nameof(ChangeScene), 0.1f);
                 f = 2;
+                Level_Floor = 1;
+                FloorDisplay(Level_Floor);
                 break;
             case 1:
                 Debug.Log("1ステージへ");
-                Invoke(nameof(ChangeScene), 5.0f);
+                Invoke(nameof(ChangeScene), 0.1f);
                 f = 1;
+                Level_Floor = 2;
+                FloorDisplay(Level_Floor);
                 break;
             case 0:
                 Debug.Log("チュートリアルステージへ");
-                Invoke(nameof(ChangeScene), 5.0f);
+                Invoke(nameof(ChangeScene), 0.1f);
                 f = 0;
+                Level_Floor = 0;
+                FloorDisplay(Level_Floor);
                 break;
         }
     }
 
+    //シーン移動
     private void ChangeScene()
     {
        switch(f)
@@ -125,14 +133,36 @@ public class StageSelectUI : MonoBehaviour
                 SceneManager.LoadScene("Finalstage");
                 break;
             case 2:
-                SceneManager.LoadScene("2stage");
+                SceneManager.LoadScene("stage2");
                 break;
             case 1:
-                SceneManager.LoadScene("3stage");
+                SceneManager.LoadScene("stage1");
                 break;
             case 0:
-                SceneManager.LoadScene("stage");
+                SceneManager.LoadScene("tutorial");
                 break;
         }
     }
+
+    //フロア表示
+    void FloorDisplay(int F)
+    {
+        F = Level_Floor;
+        //一階
+        if(F == 10)
+        {
+            Floor_Display.text ="1";
+        }
+        //チュートリアル
+        else if(F == 0)
+        {
+            Floor_Display.text = "BT" + 2;
+        }
+        else
+        {
+             Floor_Display.text = "B" + F ;
+        }
+           
+    }
+
 }
